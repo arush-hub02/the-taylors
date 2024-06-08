283f0db4a482dd46f60d6514c15be974da3cc79b
arush-hub02 <80334097+arush-hub02@users.noreply.github.com>
2024-06-08 14:41:24 +0530

    Update db.js

--- Ecommerce-Node/src/config/db.js
+++ Ecommerce-Node/src/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
+const dbHOST = process.env.DBHOST;
 
-const mongoDbUrl =
-  "mongodb+srv://arushdeepguptacse:djNi9SsK0gO1KEd4@cluster0.xx0dmwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectDb = () => {
-  return mongoose.connect(mongoDbUrl);
+  return mongoose.connect(dbHOST);
 };
 
 module.exports = { connectDb };
