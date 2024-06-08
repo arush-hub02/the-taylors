b79944ef6279f29cf2a560d1dd0a469d654c8263
arush-hub02 <80334097+arush-hub02@users.noreply.github.com>
2024-06-08 14:43:50 +0530

    Update server.js

--- Ecommerce-Node/src/server.js
+++ Ecommerce-Node/src/server.js
@@ -1,8 +1,10 @@
 const { app } = require(".");
 const { connectDb } = require("./config/db");
+require('dotenv').config();
 
-const PORT=5454;
+const PORT = process.env.PORT || 5454;
+
 app.listen(PORT,async ()=>{
     await connectDb()
-    console.log("ecommerce api listing on port ",PORT)
+    console.log("ecommerce api listing on port : "+ PORT)
 })
